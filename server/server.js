649cfbd1988ee2ca00e8f2f64b9bc06a4da023e6
Mr.Himothuy <crowngamingmanagement@gmail.com>
2025-02-02 19:54:48 -0500

    test2

--- server/server.js
+++ server/server.js
@@ -2,12 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const app = express();
-
-app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
-
-
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
+app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+
 });
 
 const PORT = process.env.PORT || 4000;
